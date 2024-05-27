b43462a933d0ed345dbf40281d3330e69779dd56
YuBinLab-QUST <111045765+YuBinLab-QUST@users.noreply.github.com>
2024-05-27 22:14:20 +0800

    Update kpca.py

--- Feature_selection/kpca.py
+++ Feature_selection/kpca.py
@@ -3,8 +3,8 @@ import pandas as pd
 from sklearn.decomposition import KernelPCA
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-# Load data from CSV file
-file_path = r'D:\PycharmProjects\my\feature extraction\RBPhunhe2.csv'  # 请替换为您的 CSV 文件路径
+
+file_path = r'' 
 data = pd.read_csv(file_path)
 data1=np.array(data)
 data=data1[:,:]
@@ -24,31 +24,7 @@ kpca_df = pd.DataFrame(data=X_kpca)
 # result_df = pd.concat([data, kpca_df], axis=1)
 
 # Save the DataFrame to a new CSV file
-kpca_df.to_csv('RBP_kPCA.csv', index=False)
+kpca_df.to_csv('', index=False)
 
 
 
-#MDS
-# import pandas as pd
-# from sklearn.manifold import MDS
-# import matplotlib.pyplot as plt
-#
-# # 读取数据，假设数据保存在 data.csv 中
-# data = pd.read_csv('ALL_protbert_DBP.csv')
-#
-# # 创建 MDS 模型，指定降维后的维度
-# mds = MDS(n_components=200)
-#
-# # 对数据进行降维
-# data_transformed = mds.fit_transform(data)
-# kpca_df = pd.DataFrame(data=data_transformed)
-#
-# # # Concatenate the kPCA results with the original data (optional)
-# # result_df = pd.concat([data, kpca_df], axis=1)
-#
-# # Save the DataFrame to a new CSV file
-# kpca_df.to_csv('MDS_DBP.csv', index=False)
-# # 可视化降维结果
-# plt.scatter(data_transformed[:, 0], data_transformed[:, 1])
-# plt.title('MDS Visualization')
-# plt.show()
